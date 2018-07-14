014c2a4cc7d78a0873c67c98b9447f5bc76550ef
rogermoka <roger.mokarzel@gmail.com>
2018-07-14 00:18:43 -0300

    Assignment 4: Like Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthorComponent } from './author/author.component';
 import { AuthorsService } from './authors.service';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { TitleCasePipe } from './title-case.pipe';
     CourseComponent,
     AuthorComponent,
     FavoriteComponent,
-    TitleCasePipe
+    TitleCasePipe,
+    LikeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/like/like.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-like',
+  templateUrl: './like.component.html',
+  styleUrls: ['./like.component.css']
+})
+export class LikeComponent implements OnInit {
+  @Input('likesCount') likesCount: number;
+  @Input('isActived') isActive: boolean;
+
+  onClick() {
+    this.likesCount += (this.isActive) ? -1 : 1;
+    this.isActive = !this.isActive;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
