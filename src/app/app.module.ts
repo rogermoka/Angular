3e1529e8e8f43e73d8efb6d953d54b680ca7363f
rogermoka <roger.mokarzel@gmail.com>
2018-07-13 23:39:52 -0300

    Assignment 3: Title Casing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { CoursesService } from './courses.service';
 import { AuthorComponent } from './author/author.component';
 import { AuthorsService } from './authors.service';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
     AuthorComponent,
-    FavoriteComponent
+    FavoriteComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
