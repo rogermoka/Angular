9dfa57d3adfae791506c26173d07ef62e76fe676
Roger Mokarzel <roger.mokarzel@gmail.com>
2018-07-19 22:42:26 -0300

    107. Creating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,11 +8,25 @@ import { Http } from '@angular/http';
 })
 export class PostsComponent {
   posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: Http){
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  constructor( private http: Http){
+    http.get(this.url)
       .subscribe(response => {
         this.posts = response.json();
       });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+
+    this.http.post(this.url, JSON.stringify(post))
+    .subscribe(response => {
+      
+      post['id'] =  response.json().id;
+      this.posts.splice(0, 0, post);
+      console.log(response.json());
+    });
   }
 }
